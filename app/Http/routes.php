7f87d3e8941d9e897699639c6f07e50e5225369b
Robinson Legaspi <robinson.legaspi@yahoo.com>
2016-08-02 23:36:12 +0800

    change login redirect path to welcome page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,8 @@
 <?php
 
 Route::group(['middleware' => 'web'], function () {
-	Route::auth(); //login system routes
+    Route::auth(); //login system routes
+    Route::get("/login", "HomeController@index"); //override login redirect path
 
 	foreach (File::allFiles(base_path() .	 '/app/repo/Routes') as $key => $partial) {
 
